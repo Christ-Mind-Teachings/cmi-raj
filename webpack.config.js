3952267503ad3cbe3738812b56be8e9dc4798991
Rick Mercer <rmercer33@gmail.com>
2021-10-09 22:00:49 -0700

    support for ftcm

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       "oe": path.resolve(__dirname, "../cmi-oe/src/js"),
       "acol": path.resolve(__dirname, "../cmi-acol/src/js"),
       "col": path.resolve(__dirname, "../cmi-col/src/js"),
+      "ftcm": path.resolve(__dirname, "../cmi-ftcm/src/js"),
       "jsb": path.resolve(__dirname, "../cmi-jsb/src/js"),
       "raj": path.resolve(__dirname, "../cmi-raj/src/js"),
       "wom": path.resolve(__dirname, "../cmi-wom/src/js"),
